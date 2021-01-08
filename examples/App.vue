8e60f00e11d3712aa0f835d2389400e78c8e2d1e
liuyaoao <623578381@qq.com>
2021-01-08 15:06:10 +0800

    vue组件库开发的模板更新

--- examples/App.vue
+++ examples/App.vue
@@ -1,41 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div style="width:100%;height:300px;">
-      <BottomGrow>
-        <div>
-          <img alt="Vue logo" src="./assets/logo.png">
-        </div>
-        <template v-slot:grow>
-          <HelloWorld msg="Welcome to Your Vue.js App"/>
-        </template>
-
-      </BottomGrow>
+    <!-- header -->
+    <div class="header">
+      <div class="logo">
+        <Logo></Logo>
+      </div>
+      <button class="toGithub" @click="toGithub">View on GitHub</button>
     </div>
+    <div class="main">
+      <!-- sidebar -->
+      <div class="sidebar">
+        <nav-side :data="navsData"></nav-side>
+      </div>
+      <div class="view page-container">
+        <router-view></router-view>
+      </div>
+    </div>
+    <!-- footer -->
+    <!-- <div class="footer"></div> -->
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import {BottomGrow} from '../../packages/index'
-
+import Logo from './components/Logo'
+import navSide from './components/menuCom'
+import navsData from './router/routerCon.json'
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
-    BottomGrow
+    Logo,
+    navSide
+  },
+  data() {
+    return {
+      navsData
+    }
+  },
+  methods: {
+    toGithub() {
+      window.location.href = 'https://github.com/sq-github/sq-ui'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  height: 100%;
+  background-color: #17171d;
+}
+.header {
+  top: 0;
+  height: 60px;
+  background-color: #121217;
+  .logo {
+    float: left;
+  }
+  .toGithub {
+    vertical-align: middle;
+    width: 120px;
+    height: 40px;
+    line-height: 40px;
+    float: right;
+    margin: 10px 28px 0 0;
+    cursor: pointer;
+    border-radius: 6px;
+    box-shadow: 0 0 8px rgb(156, 147, 147);
+    border: none;
+    font-size: 14px;
+    font-family: 'Open Sans', sans-serif;
+    background-color: #fc297f;
+    color: #fff;
+    &:hover {
+      box-shadow: 0 0 8px #fff;
+    }
+  }
+}
+.footer {
+  position: absolute;
+  height: 60px;
+  background-color: antiquewhite;
+  width: 100%;
+}
+.footer {
+  bottom: 0;
+}
+.main {
+  background-color: #17171d;
+  position: absolute;
+  bottom: 0;
+  top: 60px;
+  // padding: 0 0 20px 0;
+  width: 100%;
+  overflow: hidden;
+}
+.sidebar,
+.view {
+  overflow: auto;
+}
+.sidebar {
+  float: left;
+  height: 100%;
+  width: 200px;
+  padding: 10px 0 10px 0;
+  border-right: #000000 3px solid;
+}
+.view {
+  padding: 0 0 80px 0;
+  float: left;
+  height: calc(100% - 50px);
+  width: calc(100% - 203px);
+  overflow: auto;
 }
 </style>
--- packages/index.js
+++ packages/index.js
@@ -1,22 +1,27 @@
-import BottomGrow from './bottomGrow/index.vue'
-BottomGrow.install = function(Vue){
-  Vue.component(BottomGrow.name, BottomGrow)
+import SqButton from './SqButton'
+import SqText from './SqText'
+import SqTextDraw from './SqTextDraw'
+
+// 所有组件列表
+const components = [SqButton, SqText, SqTextDraw]
+// 定义install方法，接收Vue作为参数
+const install = function(Vue) {
+  // 判断是否安装，安装过就不继续往下执行
+  if (install.installed) return
+  install.installed = true
+  // 遍历注册所有组件
+  components.map(component => Vue.component(component.name, component))
+  // 下面这个写法也可以
+  // components.map(component => Vue.use(component))
 }
 
-const components = [
-  BottomGrow
-]
-
-const install = function(Vue){
-  if(install.installed) return
-  components.map(component => Vue.component(component.name,component))
-}
-
-if(typeof window !== 'undefined' && window.Vue){
+// 检测到Vue才执行，毕竟我们是基于Vue的
+if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 
 export default {
   install,
-  BottomGrow
+  // 所有组件，必须具有install，才能使用Vue.use()
+  ...components
 }
