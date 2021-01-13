7cf02bd68a95692c6a16cafdf5b41abbf7964e3b
liuyaoao <623578381@qq.com>
2021-01-13 15:29:13 +0800

    updates

--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,9 @@ module.exports = {
     // },
     disableHostCheck: true
   },
+  css: {
+    extract: false
+  },
   // 扩展 webpack 配置
   chainWebpack: config => {
     // @ 默认指向 src 目录，这里要改成 examples
